de3ef2e0c7c6c60f086d0dad00cb90f0c8ebb498
Patrik Vaďura <patrikvadura@icloud.com>
2024-03-29 17:29:36 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,45 @@
+const { withPayload } = require("@payloadcms/next-payload");
+const path = require("path");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withPayload(
+  {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    transpilePackages: [
+      "@payloadcms/plugin-seo",
+      "payload/components/forms",
+      "payload/components",
+    ],
+    images: {
+        remotePatterns: [
+            { hostname: 'localhost' },
+            { hostname: 'nextjs-vercel.payloadcms.com' },
+            { protocol: 'https', hostname: new URL(process.env.NEXT_PUBLIC_APP_URL).hostname },
+            { protocol: 'https', hostname: new URL(process.env.NEXT_PUBLIC_S3_ENDPOINT).hostname },
+        ],
+      // domains: [
+      //   "localhost",
+      //   "nextjs-vercel.payloadcms.com",
+      //   process.env.NEXT_PUBLIC_APP_URL,
+      //   `${process.env.NEXT_PUBLIC_S3_ENDPOINT}`.replace("https://", ""),
+      // ],
+    },
+    webpack: {
+      resolve: {
+        alias: {},
+      },
+    },
+  },
+  {
+    configPath: path.resolve(__dirname, "./payload/payload.config"),
+  }
+);
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: true,
+});
+
+module.exports =
+  process.env.ANALYZE === "true" ? withBundleAnalyzer(nextConfig) : nextConfig;
